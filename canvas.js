3c3031844a2e49f945224a19f069ce7fbd476852
Sebastiaan Hensen <64463874+s-hensen@users.noreply.github.com>
2020-04-28 15:29:17 +0200

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -55,7 +55,17 @@ window.addEventListener('mousemove',
 	y = event.y;
 })
 
-
+window.addEventListener('touchmove',
+					   function(event){
+	 if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
+        var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+        x = touch.pageX;
+        y = touch.pageY;
+    } else if (e.type == 'mousedown' || e.type == 'mouseup' || e.type == 'mousemove' || e.type == 'mouseover'|| e.type=='mouseout' || e.type=='mouseenter' || e.type=='mouseleave') {
+        x = e.clientX;
+        y = e.clientY;
+    }
+})
 window.addEventListener('resize',
 						function(event){
 	canvas.width = window.innerWidth;
